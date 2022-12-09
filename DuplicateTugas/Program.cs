b82dfde976f5572250e1ae7250bcc946a1bdbc3f
nandatar <120163119+nandatar@users.noreply.github.com>
2022-12-09 14:23:42 +0700

    Initial commit

---
+++ DuplicateTugas/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DuplicateTugas
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void TampilanUtama()
+        {
+            Console.WriteLine("==BASIC AUTHENTICATON==");
+            Console.WriteLine("1. Create User");
+            Console.WriteLine("2. Show User  ");
+            Console.WriteLine("3. Search User");
+            Console.WriteLine("4. Login User ");
+            Console.WriteLine("5. Exit");
+        }
+    }
+}
