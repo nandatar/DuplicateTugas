563fd9ae49e93a61bb5aa7197faee31fb7935a57
nandatar <120163119+nandatar@users.noreply.github.com>
2022-12-09 18:40:35 +0700

    SHOW USER, DELETE

--- DuplicateTugas/Program.cs
+++ DuplicateTugas/Program.cs
@@ -1,11 +1,21 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Linq;
 using System.Reflection.Metadata;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace DuplicateTugas
 {
     class Program
     {
+        static int id = 0;
+        static int[] ID = { };
+        static string[] FirstName = { };
+        static string[] LastName = { };
+        static string[] UserName = { };
+        static string[] Password = { };
+
         static void Main(string[] args)
         {
             TampilanUtama();
@@ -30,7 +40,7 @@ namespace DuplicateTugas
                         break;
 
                     case 2:
-                        Console.WriteLine("Show User");
+                        View();
                         break;
 
                     case 3:
@@ -71,19 +81,21 @@ namespace DuplicateTugas
             CekPassword(password);
             User(firstname, lastName, password);
             Console.WriteLine("User Success to Created!!!");
+            Console.ReadKey();
             TampilanUtama();
         }
 
-      
+
         static void User(string firstName, string lastName, string password)
         {
-            string FirstName = firstName;
-            string LastName = lastName;
-            string UserName = firstName.Substring(0, 2) + lastName.Substring(0, 2);
-            string Password = password;
-         }
+            ID = ID.Append(id).ToArray();
+            FirstName = FirstName.Append(firstName).ToArray();
+            LastName = LastName.Append(lastName).ToArray();
+            UserName = UserName.Append(firstName.Substring(0, 2) + lastName.Substring(0, 2)).ToArray();
+            Password = Password.Append(password).ToArray();
+            id++;
+        }
 
-         
 
 
         static string CekPassword(string password)
@@ -111,17 +123,83 @@ namespace DuplicateTugas
         
         static void View()
         {
-     
-           
-        }
+            Console.Clear();
+            Console.WriteLine("==SHOW USER==");
+            for (int i = 0; i < ID.Length; i++)
+            {
+                Console.WriteLine("========================");
+                Console.WriteLine($"ID	: " + (i+1));
+                Console.WriteLine("Name\t: " + FirstName[i] + " " + LastName[i]);
+                Console.WriteLine("Username: " + UserName[i]);
+                Console.WriteLine("Password: " + Password[i]);
+                Console.WriteLine("========================");
 
+            }
+            Console.WriteLine("\nMenu");
+            Console.WriteLine("1. Edit User");
+            Console.WriteLine("2. Delete User");
+            Console.WriteLine("3. Back");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    bool flag = false;
+                    while (true)
+                    {
+                        Console.Write("Id Yang Ingin Diubah : ");
+                        int y = Convert.ToInt32(Console.ReadLine());
+                        if (y > ID.Length)
+                        {
+                            Console.WriteLine("User Tidak Ditemukan!!!");
+                            flag = true;
+                        }
+                        else
+                        {
+                            Console.Write("First Name : ");
+                            FirstName[id-1] = Console.ReadLine();
+                            Console.Write("Last Name : ");
+                            LastName[id-1] = Console.ReadLine();
+                            Console.Write("Password : ");
+                            Password[id-1] = Console.ReadLine();
+                            Console.Write("User Succes to Edited !!! ");
+                            flag = false;
+                        }
+                        if (!flag)
+                        {
+                            Console.ReadKey();
+                            View();
+                            return;
+                        }
+                    }
 
+                case 2:
+                    Console.Write("Id Yang Ingin Dihapus : ");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    if (x > ID.Length)
+                    {
+                        Console.WriteLine("User Not Found!!!");
+                    }
+                    int numToRemove = x;
+                    int numIndex = Array.IndexOf(ID, numToRemove);
+                    ID = ID.Where((val, idx) => idx != numIndex).ToArray();
+                    numIndex = Array.IndexOf(FirstName, numToRemove);
+                    FirstName = FirstName.Where((val, idx) => idx != numIndex).ToArray();
+                    numIndex = Array.IndexOf(LastName, numToRemove);
+                    LastName = LastName.Where((val, idx) => idx != numIndex).ToArray();
+                    numIndex = Array.IndexOf(Password, numToRemove);
+                    Password = Password.Where((val, idx) => idx != numIndex).ToArray();
 
+                    Console.WriteLine("User Success to Deleted !!!");
+                    Console.ReadKey();
+                    View();
+                    return;
 
-
-        private void ShowUser()
-        {
-
+                case 3:
+                    TampilanUtama();
+                    return;
+            }
+            Console.WriteLine("ERROR : Input Not Valid");
+            Console.ReadKey();
+            View();
         }
 
 
